c6450eb84a3c8a6a0820c7129f8e010f73d48238
EC2 Default User <ec2-user@ip-172-31-13-10.us-west-2.compute.internal>
2017-05-19 17:56:44 +0000

    Demo API hello world to Geoserver

---
+++ src/main/groovy/Sacback.groovy
@@ -0,0 +1,23 @@
+import it.geosolutions.geoserver.rest.GeoServerRESTReader
+import it.geosolutions.geoserver.rest.GeoServerRESTPublisher
+
+class Sacback {
+ public static void main(String[] args) {
+  String RESTURL  = "http://35.166.184.150/geoserver"
+  //String RESTURL  = "http://localhost:8080/geoserver";
+  String RESTUSER = "admin";
+  String RESTPW   = "geoserver";
+        
+  GeoServerRESTReader reader = new GeoServerRESTReader(RESTURL, RESTUSER, RESTPW);
+  GeoServerRESTPublisher publisher = new GeoServerRESTPublisher(RESTURL, RESTUSER, RESTPW);
+
+  //boolean exists = reader.existsLayer("tiger", "giant_polygon");
+  //println exists
+
+  println "sacback 4"
+
+  boolean created = publisher.createWorkspace("craigWorkspace")
+  println created
+  println "sacback 99"
+ }
+}
